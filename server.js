39c2167e6dab9bc4ca9036acf3423abefad05ddc
Dianne Laguerta <diannelaguerta@gmail.com>
2017-01-02 13:10:42 -0800

    installed mongo, adding dotenv

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-// console.log("Node server is running, great!");
-
+//node server
 const express = require('express');
 const bodyParser = require('body-parser'); // express middleware for reading post requests from forms, Use with "use" method
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds151028.mlab.com:51028/zines', (err, database) => {
+  // ... start the server
+});
 
 app.listen(3000, function() {
   console.log('listening on 3000');
@@ -13,11 +17,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 //CRUD handlers below
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html');
   // Note: __dirname is directory that contains the JavaScript source code.
 });
 
 app.post('/zines', (req, res)=> {
   console.log(req.body);
-})
+});
 
